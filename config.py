cd50664326c08033be0cfe69cd612e71b7fb9238
hamza@redapesolutions.com <hamza@redapesolutions.com>
2018-07-29 16:08:18 +0800

    Add format to produce raw alias definition

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 AMAN_ALIAS_FILE = '.aliases'
 
 ALIAS_DEFINITION_REGEX = r'\s*alias (\w+)=[\'\"](.+)[\'\"]'
-ALIAS_NAME_REGEX = ''
-ALIAS_VALUE_REGEX = ''
+ALIAS_SAVING_FORMAT = '\nalias {name}="{value}"'
