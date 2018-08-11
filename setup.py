6cd006120d55e2a710e7e0ac05b5bc9997147e6e
hamza@redapesolutions.com <hamza@redapesolutions.com>
2018-08-12 00:32:33 +0800

    File config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Aman',
+    version='0.1',
+    packages=['aman',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    scripts=['test.py']
+)
