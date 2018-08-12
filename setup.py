e859c2ba421cefb01eb63b4a291fea60a8ce65ca
hamza@redapesolutions.com <hamza@redapesolutions.com>
2018-08-12 16:08:39 +0800

    Pump version to 1.0

--- setup.py
+++ setup.py
@@ -4,18 +4,26 @@ import os
 
 setup(
     name='Aman',
-    version='0.1',
+    version='1.0',
     packages=['aman',],
     license='MIT',
     long_description=open('README.md').read(),
     scripts=['bin/aman']
 )
 
-# Creating alias storage file
+# Creating directory containing aman storage file
 HOME_PATH = os.getenv('HOME')
 DIR = '.aman'
+try:
+	os.mkdir('{}/{}'.format(HOME_PATH, DIR))
+except FileExistsError:
+	pass
+
+# Creating file to store aliases
 ALIAS_FILE_NAME = 'aliases'
 ALIAS_FILE_PATH = '{}/{}/{}'.format(HOME_PATH, DIR, ALIAS_FILE_NAME)
-os.mkdir('{}/{}'.format(HOME_PATH, DIR))
-with open(ALIAS_FILE_PATH, 'w') as f:
+try:
+	with open(ALIAS_FILE_PATH, 'w') as f:
+		pass
+except FileExistsError:
 	pass
